ae422eb03a221d4dbc88489a11c44cd4f41f3ca8
Edward Marzal <edward.marzel@contenstack.com>
2023-05-09 08:29:55 -0500

    removed trackjs

--- ui/src/hooks/useJsErrorTracker.tsx
+++ ui/src/hooks/useJsErrorTracker.tsx
@@ -1,13 +1,10 @@
-import { TrackJS } from 'trackjs';
 import { datadogRum } from '@datadog/browser-rum';
 
 const useJsErrorTracker = () => {
   const addMetadata = (key: string, value: string) => {
-    datadogRum.setGlobalContextProperty(key, value);
-    TrackJS.addMetadata(key, value);
+    datadogRum.setGlobalContextProperty(key, value);   
   };
   const trackError = (error: any) => {
-    TrackJS.track(error);
     //error tracking by dataDog RUM
     datadogRum.addError(error);
   };
