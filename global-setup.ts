93e3f80b4b4826709419d44fc1bb601500aa1588
Edward Marzal <edward.marzel@contenstack.com>
2023-05-23 09:45:20 -0500

    applied feedback

--- global-setup.ts
+++ global-setup.ts
@@ -3,19 +3,22 @@ import { AppLogin } from './tests/login';
 import { getAuthToken } from './tests/pre-initialization';
 
 const globalSetup = async () => {
-  const stackEmail = process.env.CONTENTSTACK_LOGIN;
-  const stackPassword = process.env.CONTENTSTACK_PASSWORD;
+
+  const {CONTENTSTACK_LOGIN, CONTENTSTACK_PASSWORD , BASIC_AUTH_PASSWORD , BASIC_AUTH_USERNAME} = process.env;
+
+  const userstackEmail = CONTENTSTACK_LOGIN;
+  const userstackPassword = CONTENTSTACK_PASSWORD;
 
   const browser = await chromium.launch();
   const stagLogin = await browser.newPage({
     httpCredentials: {
-      username: process.env.BASIC_AUTH_USERNAME || '',
-      password: process.env.BASIC_AUTH_PASSWORD || '',
+      username: BASIC_AUTH_USERNAME || '',
+      password: BASIC_AUTH_PASSWORD || '',
     },
   });
   const loginSetup = new AppLogin(stagLogin);
   await loginSetup.getLoginPage();
-  await loginSetup.contentstackLogin(stackEmail, stackPassword);
+  await loginSetup.contentstackLogin(userstackEmail, userstackPassword);
   await getAuthToken();
 };
 
--- tests/login.ts
+++ tests/login.ts
@@ -1,5 +1,7 @@
 import { expect, Locator, Page } from '@playwright/test';
 
+const {APP_HOST_URL, CONTENTSTACK_ORGANIZATION_UID} = process.env;
+
 export class AppLogin {
   readonly page: Page;
   readonly emailInput: Locator;
@@ -14,6 +16,8 @@ export class AppLogin {
     this.venusPasswordInput = this.page.locator('#password');
     this.loginButton = this.page.locator('button:has-text("Log In"), button:has-text("LOGIN")');
   }
+
+  
   // check app url
   async checkAppUrl(url: string) {
     await expect(this.page).toHaveURL(url);
@@ -21,7 +25,7 @@ export class AppLogin {
 
   // goto login url
   async getLoginPage() {
-    await this.page.goto(`${process.env.APP_HOST_URL}`);
+    await this.page.goto(APP_HOST_URL);
   }
 
   // contentstack login
@@ -36,7 +40,7 @@ export class AppLogin {
         await this.page.locator('.user-name').click();
         await this.page.click('text=New Interface');
         await this.page.click('.OrgDropdown');
-        await this.page.click(`#${process.env.CONTENTSTACK_ORGANIZATION_UID}`);
+        await this.page.click(CONTENTSTACK_ORGANIZATION_UID);
         await this.page.waitForTimeout(2000);
         await this.page.context().storageState({ path: 'storageState.json' });
         await this.page.close();
@@ -50,7 +54,7 @@ export class AppLogin {
       const venusLoginButton = await this.page.waitForSelector('button:has-text("Log In")');
       await venusLoginButton.click();
       await this.page.click('.OrgDropdown');
-      await this.page.click(`#${process.env.CONTENTSTACK_ORGANIZATION_UID}`);
+      await this.page.click(CONTENTSTACK_ORGANIZATION_UID);
       await this.page.waitForTimeout(2000);
       await this.page.context().storageState({ path: 'storageState.json' });
       await this.page.close();
