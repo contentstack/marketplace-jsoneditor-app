ab9455c695aa8e14d5557842b47e6bff9794b78f
Abhishek Ezhava <abhishek.ezhava@contentstack.com>
2023-07-04 16:17:09 +0530

    fix:e2e test cases

--- ui/tests/e2e/json-editor.spec.ts
+++ ui/tests/e2e/json-editor.spec.ts
@@ -2,18 +2,12 @@ import { test, expect } from '@playwright/test';
 import { jsonEditorApp } from '../pages/jsoneditorpage';
 
 test.describe(" Json editor App at entry", () => {
-  let jsoneditorapp;
+  let app;
   
   test('Should Apply all the Json app tests', async ({page}) => {
-    jsoneditorapp = new jsonEditorApp(page);
-    await jsoneditorapp.openJsonApp()
-    await jsoneditorapp.fillJsonApp()
-    await jsoneditorapp.formatJson()
-    await jsoneditorapp.compactJson()
-    await jsoneditorapp.transformJson()
-    await jsoneditorapp.repairJson()
-    await jsoneditorapp.undoAction()
-    await jsoneditorapp.redoAction()
+    app = new jsonEditorApp(page);
+    await app.navigateEntryPage()
+    await app.validateJsonApp()
   });
 
 });
--- ui/tests/pages/jsoneditorpage.ts
+++ ui/tests/pages/jsoneditorpage.ts
@@ -1,4 +1,4 @@
-import { Locator, Page, Frame } from '@playwright/test';
+import { Locator, Page, Frame, expect } from '@playwright/test';
 import { elements } from '../elements/jsoneditor.elements';
 
 export class jsonEditorApp {
@@ -7,74 +7,15 @@ export class jsonEditorApp {
     this.page = page;
   }
 
-  async openJsonApp() {
+  async navigateEntryPage() {
     await this.page
-    .goto(`${process.env.ENV_URL}/#!/stack/${process.env.STACK_UID}/content-type/test_json_editor/en-us/entry/create`);
-    
+      .goto(`${process.env.ENV_URL}/#!/stack/${process.env.STACK_UID}/content-type/test_json_editor/en-us/entry/create`);
   }
 
-  async fillJsonApp(){
-
-    await this.page.frameLocator('[data-testid="app-extension-frame"]').locator('div').filter({ hasText: /^\{\}$/ }).nth(1).click();
-
-    await this.page
-    .frameLocator('[data-testid="app-extension-frame"]')
-    .getByRole('textbox')
-    .press('ArrowLeft');
-
-    await this.page
-    .frameLocator(elements.FrameLocator)
-    .getByRole('textbox')
-    .fill('date : today');
-
-    await this.page
-      .frameLocator(elements.FrameLocator)
-      .locator('div')
-      .filter({ hasText: /^\{date : today\}$/ })
-      .nth(1)
-      .click();
-  }
-
-  async formatJson(){
-    await this.page
-      .frameLocator(elements.FrameLocator)
-      .getByRole('button', { name: elements.formatJsonName })
-      .click();
-  }
-  
-  async compactJson(){
-    await this.page
-      .frameLocator(elements.FrameLocator)
-      .getByRole('button', { name: elements.compactJsonName })
-      .click();
-  }
-
-  async transformJson(){
-    await this.page
-      .frameLocator(elements.FrameLocator)
-      .getByRole('button', { name: elements.transformJsonName})
-      .click();
-  }
- 
-  async repairJson(){
-    await this.page
-      .frameLocator(elements.FrameLocator)
-      .getByRole('button', { name: elements.repairJsonName })
-      .click();
-  }
-
-  async undoAction(){
-    await this.page
-      .frameLocator(elements.FrameLocator)
-      .getByRole('button', { name:  elements.undoJsonAction })
-      .click();
-  }
-
-  async redoAction(){
-    await this.page
-      .frameLocator(elements.FrameLocator)
-      .getByRole('button', { name: elements.redoJsonAction })
-      .click();
+  async validateJsonApp() {
+    const appFrameLocator = await this.page.frameLocator('[data-testid="app-extension-frame"]')
+    await appFrameLocator.locator('div').filter({ hasText: /^\{\}$/ }).nth(1).click();
+    await expect(appFrameLocator.locator('div').filter({ hasText: /^\{\}$/ }).nth(1)).toBeVisible();
   }
 
 };
